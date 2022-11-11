f3a0c29d1890a9adae3ff2b4314ae01c5231d209
yyy333yyy <h0abzu.emyaa3483@gmail.com>
2022-11-11 18:07:49 +0900

    ハッシュロケットをシンボルに修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -38,7 +38,7 @@ class CalendarsController < ApplicationController
       if wday_num > 7
         wday_num = wday_num -7
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :wday => wdays[wday_num], :plans => today_plans}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, wday: wdays[wday_num], plans: today_plans}
       @week_days.push(days)
     end
 
